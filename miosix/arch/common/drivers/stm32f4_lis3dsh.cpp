74eafde1a355f8b7db26780322fca6f21ec688c2
Raffaele Fioratto <raffaele.fioratto@gmail.com>
2016-12-21 18:49:58 +0100

    [Accelerometer] Code for SPI initialization
    The code initializes the SPI bus, first by configuring the master with
    the appropriate parameters and then by actually enabling the bus.
    The accelerometer communicates to the microcontroller using this protocol.
    
    Signed-off-by: Raffaele Fioratto <raffaele.fioratto@gmail.com>

--- miosix/arch/common/drivers/stm32f4_lis3dsh.cpp
+++ miosix/arch/common/drivers/stm32f4_lis3dsh.cpp
@@ -33,10 +33,64 @@
 
 namespace miosix {
     
+/* ----------------- SPI Defines ----------------- */
+    
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_2	(0x00 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_4	(0x01 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_8	(0x02 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_16	(0x03 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_32	(0x04 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_64	(0x05 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_128	(0x06 << 3)
+#define SPI_CR1_BAUDRATE_FPCLK_DIV_256	(0x07 << 3)
+  
+#define SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE	(0 << 1)
+#define SPI_CR1_CPOL_CLK_TO_1_WHEN_IDLE	(1 << 1)
+    
+#define SPI_CR1_CPHA_CLK_TRANSITION_1	(0 << 0)
+#define SPI_CR1_CPHA_CLK_TRANSITION_2	(1 << 0)
+    
+#define SPI_CR1_DFF_8BIT                (0 << 11)
+#define SPI_CR1_DFF_16BIT               (1 << 11)
+    
+#define SPI_CR1_MSBFIRST		(0 << 7)
+    
+/* ------------------- Local Typedefs ------------------------*/
+    
 typedef Gpio<GPIOA_BASE,5> lis3dsh_CLK;
 typedef Gpio<GPIOA_BASE,6> lis3dsh_MOSI;
 typedef Gpio<GPIOA_BASE,7> lis3dsh_MISO;
 typedef Gpio<GPIOE_BASE,3> lis3dsh_CS;
+
+/* ------------ Local functions implementation -------------- */
+
+static void spi_enable(SPI_TypeDef* SPI)
+{
+    SPI->CR1 |= SPI_CR1_SPE; /* Enable SPI. */
+}
+
+
+static int spi_init_master(SPI_TypeDef* SPI, uint32_t br, uint32_t cpol, uint32_t cpha,
+		    uint32_t dff, uint32_t lsbfirst)
+{
+    uint32_t reg32 = SPI->CR1;
+
+    /* Reset all bits omitting SPE, CRCEN and CRCNEXT bits. */
+    reg32 &= SPI_CR1_SPE | SPI_CR1_CRCEN | SPI_CR1_CRCNEXT;
+
+    reg32 |= SPI_CR1_MSTR;	/* Configure SPI as master. */
+
+    reg32 |= br;		/* Set baud rate bits. */
+    reg32 |= cpol;		/* Set CPOL value. */
+    reg32 |= cpha;		/* Set CPHA value. */
+    reg32 |= dff;		/* Set data format (8 or 16 bits). */
+    reg32 |= lsbfirst;          /* Set frame format (LSB- or MSB-first). */
+
+    SPI->CR2 |= SPI_CR2_SSOE;   /* common case */
+    SPI->CR1 = reg32;
+
+    return 0;
+}
 
 /**
  * \internal
@@ -69,9 +123,36 @@ static void gpio_init()
     lis3dsh_CS::high();
 }
 
+/**
+ * \internal
+ * Initialize SPI
+ */
+static void spi_init()
+{
+    /* Enable SPI1 clock. */
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+
+    /* Pulse SPI1 reset line */
+    RCC->APB2RSTR |= RCC_APB2RSTR_SPI1RST;
+    RCC->APB2RSTR &= ~RCC_APB2RSTR_SPI1RST;
+            
+    /* Initialize SPI1 master */
+    spi_init_master(SPI1,
+                    SPI_CR1_BAUDRATE_FPCLK_DIV_64,
+                    SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
+                    SPI_CR1_CPHA_CLK_TRANSITION_1,
+                    SPI_CR1_DFF_8BIT,
+                    SPI_CR1_MSBFIRST);
+    
+    /* Enable SPI1 first */
+    spi_enable(SPI1);
+}
+
 SPILIS3DSHDriver::SPILIS3DSHDriver() : Device(Device::TTY) {
     
     gpio_init();
+    
+    spi_init();
 }
 
 intrusive_ref_ptr<SPILIS3DSHDriver> SPILIS3DSHDriver::instance() {
